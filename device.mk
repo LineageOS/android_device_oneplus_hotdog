1f180c64d64b9860737cc0af85cccc5826672ea7
LuK1337 <priv.luk@gmail.com>
2021-04-02 17:12:42 +0200

    Revert "hotdog: Build fastbootd"
    Moved to sm8150-common.
    
    This reverts commit d63cfab014810b83d096fe453edf1c8d3199c48c.
    
    Change-Id: Ied0c449cffbbc856d00c391fe18ad8229462c107

--- device.mk
+++ device.mk
@@ -39,10 +39,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
