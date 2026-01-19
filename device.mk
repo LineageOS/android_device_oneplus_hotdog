e0f3e9d17ec069ba7c9fd9d1be2dab08e0c03a48
Bruno Martins <bgcngm@gmail.com>
2026-01-19 00:21:31 +0000

    hotdog: Create standalone RRO for KeyHandler
    Change-Id: Ia6867ce4443de4edfd54dc61db5550fc3a59ccd8

--- device.mk
+++ device.mk
@@ -32,6 +32,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    KeyHandlerResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
